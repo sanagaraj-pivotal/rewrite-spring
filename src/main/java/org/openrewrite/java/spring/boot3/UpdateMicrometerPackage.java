896c4454fdda6d0215977cb2f06422c0667e288f
Andrei Shakirin <andrei.shakirin@gmail.com>
2022-07-12 15:41:24 +0200

    Added test for update micrometer package

---
+++ src/main/java/org/openrewrite/java/spring/boot3/UpdateMicrometerPackage.java
@@ -0,0 +1,10 @@
+package org.openrewrite.java.spring.boot3;
+
+import org.openrewrite.Recipe;
+
+public class UpdateMicrometerPackage extends Recipe {
+    @Override
+    public String getDisplayName() {
+        return "UpdateMicrometerPackage";
+    }
+}
